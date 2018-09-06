2c19e016a979d69567d0821ea1866a0e11943df9
Nick Bates <19batesnicholas@gmail.com>
2018-09-06 09:02:25 -0700

    Pikachu project

---
+++ Pikachu/src/pikachu.java
@@ -0,0 +1,8 @@
+public class pikachu {
+    public static void main(String[] args){
+
+        System.out.println("   (\\__/)");
+        System.out.println("   (*^.^)");
+        System.out.println("z(_(\")(\")");
+    }
+}
