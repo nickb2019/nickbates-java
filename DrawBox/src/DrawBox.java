f46ac28c08af9b164c59a320e01d4a4eecdb71b1
Nick Bates <19batesnicholas@gmail.com>
2018-09-10 08:59:42 -0700

    Draw Box

---
+++ DrawBox/src/DrawBox.java
@@ -0,0 +1,30 @@
+public class DrawBox {
+    public static void main(String[] args){
+        //draw box
+
+        drawBox();
+        drawBox();
+        drawBox();
+        System.out.println("Complete!");
+
+    }
+
+    public static void drawBox(){
+        drawTopBottom();
+        drawSides();
+        drawTopBottom();
+
+
+
+    }
+
+    public static void drawTopBottom(){
+        System.out.println("+-------+");
+    }
+
+    public static void drawSides(){
+        System.out.println("|       |");
+        System.out.println("|       |");
+    }
+
+}
