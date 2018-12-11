dfa5cffc0dbaa8b08669310d2af4b845181f23ad
Nick Bates <19batesnicholas@gmail.com>
2018-12-11 09:18:47 -0800

    oo

---
+++ Looper/src/Looper.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Looper {
+    public static void main(String[] args) {
+        int w = 0;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter Something: ");
+        String input = scan.next();
+        System.out.println(input);
+    }
+}
--- Rolldice/src/rollDice.java
+++ Rolldice/src/rollDice.java
@@ -1,9 +1,13 @@
+//Nicholas Bates
+//12/7/18
+//Ap CS A
 import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Scanner;
 public class rollDice {
     public static final int DICENUM = 3;
     public static void main(String[] args) {
+        //asks user if they want to play
         String play = " ";
         System.out.println("Do you want to roll a dice? y/n?");
         Scanner playGame = new Scanner(System.in);
@@ -12,19 +16,21 @@ public class rollDice {
         String gameDice[][] = new String[DICENUM][5];
         dice = defDice();
         while(play.equals("y")) {
+            //makes it roll to the class constant
             for(int i=0;i<DICENUM;i++) {
                 int x = rollDice();
                 printDice(dice, x);
                 drawLine();
             }
+            //asks user if they want to continue
             System.out.println("Do you want to Roll Again y/n?");
             play = playGame.nextLine();
 
         }
     }
 
+   //defines the 6 sided dice
     public static String[][] defDice() {
-        System.out.println("Im at defDice");
          return new String [][]{
                 {" ------- ",
                         "|       |",
@@ -60,6 +66,7 @@ public class rollDice {
         };
 
     }
+    //prints dice
     public static void printDice(String[][] dice, int x){
 
 
@@ -70,13 +77,14 @@ public class rollDice {
        }
 
 
-    public static int rollDice() {
+    //rolls the dice
+       public static int rollDice() {
 
         int x = (int)(Math.random()*5)+1;
 
             return x;
     }
-
+//creates a line
     public static void drawLine(){
 
         System.out.println("---------------------------");
